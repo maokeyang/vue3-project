6df9e93f3c785613f66681e39ee4cab5f0dab5d7
maokeyang <maokeyang1031@gmail.com>
2023-08-13 23:39:27 +0800

    feat: add tab

---
+++ src/views/tabs/TabsView.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { RouterView, useRoute, useRouter } from 'vue-router'
+import { Tabbar, TabbarItem } from 'vant';
+import { ref, watch } from 'vue'
+
+const route = useRoute()
+const router = useRouter()
+// 记录当前选择的tab
+const active = ref(route.name as string)
+
+
+watch(active, (nv) => {
+    router.push(nv)
+})
+
+</script>
+
+<template>
+    <div class="center">{{ active }}</div>
+    <Tabbar v-model="active">
+      <TabbarItem name="home" icon="home-o">首页</TabbarItem>
+      <TabbarItem name="order" icon="bars">订单</TabbarItem>
+      <TabbarItem name="me" icon="contact">我的</TabbarItem>
+    </Tabbar>
+</template>
+
+
+<style scoped>
+.center {
+    text-align: center;
+}
+</style>
