0b29ef8b6d7ec6a140b386b5df756784fd06ef85
wanghan143 <wanghan143@jd.com>
2023-08-16 20:08:21 +0800

    feat: 添加全局函数和变量

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,28 @@ import 'vant/lib/index.css'
 
 const app = createApp(App)
 
+// 定义全局变量
+app.config.globalProperties.$env = 'dev'
+
+app.config.globalProperties.$filters = {
+    fomart<T>(str:T) {
+        return 'test_' + str;
+    }
+}
+
+type Filter = {
+    fomart<T>(str: T) : string
+}
+
+declare module 'vue' {
+    export interface ComponentCustomProperties {
+        $filters: Filter,
+        $env: string
+    }
+}
+ 
+
+
 app.use(createPinia())
 app.use(router)
 
--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -3,7 +3,9 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
+  // 相当于getter
   const doubleCount = computed(() => count.value * 2)
+  // 相当于action
   function increment() {
     count.value++
   }
