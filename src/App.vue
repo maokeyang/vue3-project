b165fb30e3085abc518e6f36139f6a6851703a03
maokeyang <maokeyang1031@gmail.com>
2023-08-20 14:47:26 +0800

    feat: 样式初始化

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,10 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
+  @import 'normalize.css'
+  /** 这个属性可以使页面上的字体抗锯齿,使用后字体看起来会更清晰舒服 */
+  #app {
+    -webkit-font-smoothing: antialiased; 
+    -moz-osx-font-smoothing: grayscale;
+  }
 </style>
